77f03618c5dd4ca08fb73f27e42a60aa33eb9527
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-06-22 21:03:44 -0300

    feat: created global item value changed event on tag collection

--- DotNetCom/General/Tags/TagCollection.cs
+++ DotNetCom/General/Tags/TagCollection.cs
@@ -1,6 +1,7 @@
 ﻿using DotNetCom.DataBase;
 using DotNetCom.General.NamedObject;
 using Newtonsoft.Json;
+using System;
 using System.ComponentModel;
 using System.Drawing.Design;
 
@@ -19,5 +20,30 @@ namespace DotNetCom.General.Tags
         [Browsable(false)]
         public Tag[] Tags { get => Data.TagsDataBase.GetTags(Names); }
 
+        public event EventHandler TagChanged;
+
+        private void Item_ValueChanged(object sender, EventArgs e)
+        {
+            TagChanged?.Invoke(sender, e);
+        }
+
+        public void EnableEvents()
+        {
+            var tags = Tags;
+            DisableEvents();
+            foreach (var item in tags)
+            {
+                item.ValueChanged += Item_ValueChanged;
+            }
+        }
+
+        public void DisableEvents()
+        {
+            var tags = Tags;
+            foreach (var item in tags)
+            {
+                item.ValueChanged -= Item_ValueChanged;
+            }
+        }
     }
 }
